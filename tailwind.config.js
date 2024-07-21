3642faf6d167d14e048dce99c97135bcca08030f
Utsav330 <utsavtowards@gmail.com>
2024-07-21 03:35:02 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./projects/index.html"],
+  content: ["./Another/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
